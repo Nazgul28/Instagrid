4cb50c3e2b69bbc18849a6a05f0f9dc624e34c1d
Nazgul28 <lfmassola@gmail.com>
2015-10-07 17:00:59 -0700

    project 3 gulp

--- gulpfile.js
+++ gulpfile.js
@@ -2,12 +2,49 @@
 
 var gulp = require('gulp');
 var uglify = require('gulp-uglify');
+var concat = require('gulp-concat');
+var browserSync = require('browser-sync').create();
+var connect = require('gulp-connect');
 
-gulp.task('default', function(){
+gulp.task('compress-js', function(){
 	
-	gulp.src('JS/*.js')
+	gulp.src('js/*.js')
+		.pipe(concat('main.min.js'))
 		.pipe(uglify())
-		.pipe(gulp.dest('JS'));
+		.pipe(gulp.dest('build/js'))
 
 });
+
+gulp.task('connect', function(){
+	connect.server();
+});
+
+gulp.task('compress-css', function(){
+	
+	gulp.src('css/*.css')
+	.pipe(concat('main.css'))
+	.pipe(gulp.dest('build/css'))
+
+});
+
+gulp.task('watch', function(){
+	gulp.watch(['js/*.js'], ['compress-js']); 
+	gulp.watch(['css/*.css'], ['compress-css']);
+});
+
+// Static server
+gulp.task('browser-sync', function() {
+    browserSync.init({
+        server: {
+            baseDir: "./"
+        }
+    });
+gulp.watch(["index.html", "css/*.css", "js/*.js"]).on('change', browserSync.reload);
+
+});
+
+// or...
+
+
+
 
